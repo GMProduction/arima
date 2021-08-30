8ec7d625740ab9ebde9f0e68ef63b21ed4d46372
pradana <pradanamahendra@gmail.com>
2021-08-30 17:00:38 +0700

    asd

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Login | Prediksi toko roti menggunakan arma</title>
+    <title>Login | Prediksi UD.SANUSI menggunakan arima</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
